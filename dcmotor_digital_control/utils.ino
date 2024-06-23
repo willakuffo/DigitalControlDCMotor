94539eb8bf308a0eacb94126f939d893bb703070
willakuffo <william.hawkins71@gmail.com>
2024-06-23 16:54:52 +0000

    assigned right filenames

--- dcmotor_digital_control/utils.ino
+++ dcmotor_digital_control/utils.ino
@@ -1,80 +1,101 @@
-
-
 #include <stdint.h>
 #include "utils.h"
 
-#define A0 1.0000
-#define A1 -0.9983
-#define A2 -0.001747
-#define B0  0.02156
-#define B1 0.04312
-#define B2  0.02156
 
-float yk = 0, yk_1 = 0, yk_2 = 0, uk = 0, uk_1 = 0, uk_2 = 0;
-float uks[3] = { 0, 0, 0 };
-float yks[3] = { 0, 0, 0 };
-unsigned long k = 0;  //dicrete time index
+uint8_t pwm(uint8_t pin, float duty_cycle, float frequency) {
+  unsigned long pwm_period = (1 / frequency) * 1e6;
+  unsigned long current_time = micros();
+  static unsigned long start_time = current_time;
+  static uint8_t pwm_state;
 
-void pushBack(float x_new, float *x_array, uint8_t array_size);
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(pwm_pin1, OUTPUT);
-  pinMode(pwm_pin2, OUTPUT);
-  setup_encoder_ISR(enc_CHA, enc_CHB);
+  unsigned long on_duration = (duty_cycle / 100) * pwm_period;
+  unsigned long off_duration = pwm_period - on_duration;
 
 
-  Serial.begin(9600);
-}
+  //(current_time - )%pwm_period >= 0? trigger_off_state = 1: trigger_on_state = 0;
+  switch (pwm_state) {
+    case 0:
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  unsigned long start_time = micros();
+      if ((current_time - start_time) >= off_duration) {
+        digitalWrite(pin, HIGH);
+        pwm_state = 1;
+        start_time = current_time;
+      }
 
-  //uint8_t pwm_state = pwm(pwm_pin1,50,200);
-
-  float max_min_amp[2] = { -5.0, 5.0 };
-  uk = square_wave(max_min_amp, 50, 0.2);
-  cmd_dc_motor(uk, max_min_amp);
-  //analogWrite(pwm_pin1, 128);
-  //pwm(pwm_pin1, 100, 100);
-  //analogWrite(pwm_pin2, 0);
-
-
-
-
-
-
-  //Serial.print(micros()-start_time);Serial.print(" ");Serial.print(sizeof(6.909e-9));Serial.print(" ");Serial.println(enc_pulses_count);
-  //Serial.print(yks[2]);Serial.print(" ");Serial.print(yks[1]);Serial.print(" ");Serial.print(yks[0]);Serial.print(" ");
-  //Serial.print(uks[2]);Serial.print(" ");Serial.print(uks[1]);Serial.print(" ");Serial.println(uks[0]);
-  //uk = 5.0*0.5;//*pwm_state;
-
-  yk = (B0 / A0) * uks[0] + (B1 / A0) * uks[1] + (B2 / A0) * uks[2] - (A1 / A0) * yks[1] - (A2 / A0) * yks[2];
-  pushBack(uk, uks, sizeof(uks) / sizeof(uks[0]));
-  pushBack(yk, yks, sizeof(yks) / sizeof(yks[0]));
-
- //delay(50); 
-
-
-
-  //delay(50);//adjust sample time
-  //Serial.print("ts[us1]:");
-  //Serial.print(0);
-  Serial.print(" ts[us]:");
-  Serial.print(micros() - start_time);
-  Serial.print(" u[k]:");
-  Serial.print(uk);
-  Serial.print(" y[k]-rad:");
-  Serial.print(yk);
-  Serial.print(" dc-rad:");
-  Serial.println(((float)enc_pulses_count / (float)enc_counts_per_rev) * 2 * PI);
-}
-
-void pushBack(float x_new, float *x_array, uint8_t array_size) {
-  for (uint8_t i = array_size - 1; i > 0; i = i - 1) {
-    x_array[i] = x_array[i - 1];
+      break;
+    case 1:
+      if ((current_time - start_time) >= on_duration) {
+        digitalWrite(pin, LOW);
+        pwm_state = 0;
+        start_time = current_time;
+      }
+      break;
   }
 
-  x_array[0] = x_new;
-};
+  return pwm_state;
+}
+
+float square_wave(float *amplitude, float duty_cycle, float frequency) {
+  unsigned long pwm_period = (1 / frequency) * 1e6;
+  unsigned long current_time = micros();
+  static unsigned long start_time = current_time;
+  static float signal;
+  static uint8_t sig_state;
+
+  unsigned long on_duration = (duty_cycle / 100) * pwm_period;
+  unsigned long off_duration = pwm_period - on_duration;
+
+
+  //(current_time - )%pwm_period >= 0? trigger_off_state = 1: trigger_on_state = 0;
+  sig_state = signal > (amplitude[0]+amplitude[1])/2;
+  switch (sig_state) {
+    case 0:
+
+      if ((current_time - start_time) >= off_duration) {
+        signal = amplitude[1];
+        start_time = current_time;
+      }
+
+      break;
+    case 1:
+      if ((current_time - start_time) >= on_duration) {
+        signal = amplitude[0];
+        start_time = current_time;
+      }
+      break;
+  }
+
+  return signal;
+}
+
+void cmd_dc_motor(float voltage,float *min_max_limit) {
+  uint8_t direction = voltage > 0 ? 1 : 0;
+  switch (direction) {
+    case 0:
+      analogWrite(pwm_pin1,abs(voltage/min_max_limit[1])*255);
+      //pwm(pwm_pin1, 100, 100);
+      analogWrite(pwm_pin2, 0);
+
+      break;
+
+    case 1:
+      analogWrite(pwm_pin2, abs(voltage/min_max_limit[0])*255);
+      //pwm(pwm_pin1, 100, 100);
+      analogWrite(pwm_pin1, 0);
+      break;
+  }
+}
+
+void setup_encoder_ISR(uint8_t enc_pinA, uint8_t enc_pinB) {
+  attachInterrupt(digitalPinToInterrupt(enc_pinA), CHA_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(enc_pinB), CHB_ISR, RISING);
+}
+
+void CHA_ISR() {
+  digitalRead(enc_CHB) ? enc_pulses_count += 0 : enc_pulses_count += 1;
+}
+
+
+void CHB_ISR() {
+  digitalRead(enc_CHA) ? enc_pulses_count += 0 : enc_pulses_count -= 1;
+}
